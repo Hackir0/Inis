97934481e7bc16ce9969ea7582fd356d7dedd6e2
Евгений <zheka0812w@mail.ru>
2025-03-17 18:01:11 +0300

    lab2

--- lab2/script.js
+++ lab2/script.js
@@ -2,16 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const shirtContainer = document.getElementById("shirt-container");
 
     shirts.forEach(shirt => {
-        // Получаем доступные цвета и изображение для отображения
         const availableColors = Object.keys(shirt.colors);
         const primaryColor = availableColors.length > 0 ? availableColors[0] : null;
         const displayImage = primaryColor ? shirt.colors[primaryColor].front : shirt.default.front;
 
-        // Создаем карточку для футболки
         const shirtCard = document.createElement("div");
         shirtCard.classList.add("shirt-card");
 
-        // Заполняем карточку содержимым
         shirtCard.innerHTML = `
             <img src="${displayImage}" alt="${shirt.name}" class="shirt-image">
             <h2>${shirt.name}</h2>
@@ -20,7 +17,6 @@ document.addEventListener("DOMContentLoaded", () => {
             <button class="see-page">Подробнее</button>
         `;
 
-        // Добавляем карточку в контейнер
         shirtContainer.appendChild(shirtCard);
     });
 });
